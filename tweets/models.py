5f69eff81b5e3c778dd229d5bc448ef3d2428501
dimson tenke <dimsontenke@dimsons-MacBook-Pro.local>
2019-02-13 22:06:03 -0500

    updated models

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.db import models
 
 class Tweet(models.Model):
-    tweet = models.Textfield()
-    update = models.datetimefield(auto_now = True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=140)
+    update = models.DateTimeField(auto_now = True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.content)
 
