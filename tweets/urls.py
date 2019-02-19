8c1f26ea6651f8f3aea4c3e1a23d3f889d99f4e5
dimson tenke <dimsontenke@dimsons-MacBook-Pro.local>
2019-02-19 00:57:46 -0500

    added Bootstrap Media Object for ListView

--- tweets/urls.py
+++ tweets/urls.py
@@ -5,7 +5,7 @@ from .views import TweetDetailView,TweetListView, TweetCreateView, TweetUpdateVi
 
 urlpatterns = [
    # url(r'^admin/', admin.site.urls),
-    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^$', TweetListView.as_view(), name='list'),#/tweet/
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),#tweet/1
     url(r'^create/$', TweetCreateView.as_view(), name='create'),#/tweet/create
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),#/tweet/1/update
