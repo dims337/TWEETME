45393bd12048e541c781758d7edb268efafb9df3
dimson tenke <dimsontenke@dimsons-MacBook-Pro.local>
2019-02-21 22:48:35 -0500

    created api view and ajax

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from django.views.generic.base import RedirectView
 
-from .views import TweetListAPIView
+from .views import TweetListAPIView, TweetCreateAPIView
 
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),#/tweet/
     url(r'^$', TweetListAPIView.as_view(), name='list'),#api/tweet/
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),#tweet/1
-    # url(r'^create/$', TweetCreateView.as_view(), name='create'),#/tweet/create
+    url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),#/tweet/create
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),#/tweet/1/update
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),#/tweet/1/delete
 ]
